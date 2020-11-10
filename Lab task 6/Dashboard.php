fa1f9645b09c9659abe75ad344c0e05d35d49efb
Nushrat Jahan <nushratjahan296@gmail.com>
2020-11-10 16:57:10 +0600

    2nd commit

---
+++ Lab task 6/Dashboard.php
@@ -0,0 +1,52 @@
+<html>
+	<head><title>
+		Dashboard
+	</title></head>
+	<body>
+				<img src="resources/headerlogo1">
+				<img src="resources/headerlogo1">
+				<table border="2"> 
+					<tr>
+						<td>
+							<a href="https://www.w3schools.com/">Add New Book</a>
+						</td>
+					</tr>
+						<td> SR.NO</td>
+						<td>NAME</td>
+						<td>PUBLISHER</td>
+						<td>ISBN</td>
+						<td>PRICE</td>
+						<td>IMAGE</td>
+						<td>DELETE</td>
+					</tr>
+					</tr>
+						<td> 1</td>
+						<td>A dictionary of</br> Architecture</td>
+						<td>THE BOOK SHOP</td>
+						<td>8964636</td>
+						<td>500</td>
+						<td><img src="resources/headerlogo1"></td>
+						<td>X</td>
+					</tr>
+					</tr>
+						<td> 2</td>
+						<td>Elephant Kingdom:</br>Sculpture from Indian</br> Architecture</td>
+						<td>Vikramjit Ram</td>
+						<td>81-88204-6</td>
+						<td>1000</td>
+						<td><img src="resources/headerlogo1"></td>
+						<td>X</td>
+					</tr>
+					</tr>
+						<td> 3</td>
+						<td>Close to Events-Works of</br>Bikash Bhattacharjee</td>
+						<td>Manasij</br>Majumder</td>
+						<td>978818973</td>
+						<td>2000</td>
+						<td><img src="resources/headerlogo1"></td>
+						<td>X</td>
+					</tr>
+					
+				</table>
+	</body>
+</html>
